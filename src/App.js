b61d2aeec50111b141d11239369001f414ceb7cf
Rijackson-son <rijackkson@gmail.com>
2024-11-20 11:27:31 -0300

    adição da pagina home

--- src/App.js
+++ src/App.js
@@ -10,12 +10,19 @@ import "./css/App.css"
 //screen
  import Header from './screens/header';
  import Sidebar from './screens/sidebar';
+ import HomePage from './screens/home';
 
 function App() {
   return (
     <div className="App flex">         
       
       <Sidebar />
+      
+      <div className="app-content">
+      
+      <HomePage />
+
+      </div>
 
      
     </div>
--- src/screens/sidebar.js
+++ src/screens/sidebar.js
@@ -20,7 +20,7 @@ function Sidebar(){
     for(let i = 0; i < nav.length; i++){
         navigation.push(
             <li key={"nav-" + i + "-" + nav[i].slug}>
-            <a href={nav[i].slug} className={"link noul flex c333" + (currentPage == nav[i].slug ? " on " : "")}>
+            <a href={nav[i].slug} className={"aic link noul flex c333" + (currentPage == nav[i].slug ? " on " : "")}>
                 <div className={"ico s20 " + nav[i].icon} />                        
                 <h2 className="lbl s20">{nav[i].label}</h2>
 
@@ -30,7 +30,7 @@ function Sidebar(){
     }
 
     return (
-        <div className="sidebar fixed">
+        <div className="sidebar rel">
             <a href="#" className="logo bl">
                 <img src={Logo} className="bl" />
             </a>
@@ -39,11 +39,37 @@ function Sidebar(){
                 {navigation}
             </ul>
 
-            <div className="updated-course">
-                <div className="icon-lamp-bright ico" />
+            <div className="updated-course flex aic">
+                <div className="icon-fire cfff fs24 ico" />
                 <div className="lbl s15 fontb c333"> 
                     Updated Courses
                     <h2 className="outhor s13 c777">by Farol Assessoria</h2>
+                </div>
+            </div>
+
+            <div className="stats aic flex">
+
+                <div className="stats-box flex aic">
+                    <div className="ico ico-points s24 icon-shield" />
+                    <h2 className="val s15 fontn">1800</h2>
+                    <h2 className="lbl s13 c777">Points</h2>
+                </div>
+
+                <div className="stats-box flex aic">
+                    <div className="ico ico-power s24 icon-power" />
+                    <h2 className="val s15 fontn">45.3%</h2>
+                    <h2 className="lbl s13 c777">Complete</h2>
+                </div>
+            </div>
+
+
+            <div className="me flex aic">
+                <div className="photo cfff fs24">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpA6Aeg0CYaaTXAKiUUu7A44g_DLYkQ8XTGQ&s" className="bl"/>
+                    </div>
+                <div className="lbl s15 fontb c333"> 
+                    RijackDev
+                    <h2 className="uname s13 c777">@rijackk</h2>                   
                 </div>
             </div>
 
